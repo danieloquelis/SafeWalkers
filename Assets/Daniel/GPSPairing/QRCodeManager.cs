f2632eaad6326788c04e01a806fe136cf0fd4b7c
Daniel Oquelis <daniel.oquelis@gmail.com>
2025-11-26 01:10:56 +0100

    chore: add feedback when pairing completed

--- Assets/Daniel/GPSPairing/QRCodeManager.cs
+++ Assets/Daniel/GPSPairing/QRCodeManager.cs
@@ -1,14 +1,17 @@
 using Meta.XR.MRUtilityKit;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class QRCodeManager : MonoBehaviour
 {
+    public UnityEvent<string> OnQRDetected;
     public void OnTrackableAdded(MRUKTrackable trackable)
     {
         if (trackable.TrackableType == OVRAnchor.TrackableType.QRCode && trackable.MarkerPayloadString != null)
         {
             var payload = trackable.MarkerPayloadString;
             Debug.Log($"[QRPayload]: {trackable.MarkerPayloadString}");
+            OnQRDetected?.Invoke(payload);
         }
     }
 }
